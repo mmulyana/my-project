ab938439e09e83d2c03d3147152c514f804d04e3
mmulyana <mulyan.t20@gmail.com>
2024-03-19 11:00:00 +0700

    feat: task model

---
+++ prisma/migrations/20240511154214_create_table_tasks/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `dueDate` DATETIME(3) NULL,
+    `isDone` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
